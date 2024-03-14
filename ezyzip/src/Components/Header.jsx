6da3624b230962fcccbf88645db7e6eb4447e068
flakhal <162927105+flakhal@users.noreply.github.com>
2024-03-14 14:45:44 +0100

    Update Header.jsx

--- ezyzip/src/Components/Header.jsx
+++ ezyzip/src/Components/Header.jsx
@@ -2,13 +2,13 @@ import react from 'react';
 import { IoNotificationsOutline } from "react-icons/io5";
 import { FaUserCircle } from "react-icons/fa";
 import './Header.css'
-import Logo from '../Assets/Logo.png'
+import Logo from '../Assets/Logosec.png'
 
 const Header = () => {
   return (
    <div className='container'>
 <div className='box1'>
-    <div className='logo'><img src='Logo' /></div> 
+    <div className='logo'><img src='Logosec' /></div> 
     <div className='em'>Gestion des employés</div>
 </div>
 <div className='box2'>
