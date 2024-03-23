2bc40fda7b6922b3f8c8cdd0bb9c871170698e1a
flakhal <162927105+flakhal@users.noreply.github.com>
2024-03-23 16:10:33 +0100

    Update Motif.jsx

--- ezyzip/src/Components/Motif.jsx
+++ ezyzip/src/Components/Motif.jsx
@@ -11,11 +11,23 @@ const Motif=({closeMotif , handleRedClick})=>{
        };
 
     
+       const [text, setText] = useState(''); 
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+    
 return(
     <div className="motifwrapper">
 <div  className="motif">
    <p>Motif de refus</p>
-   <input type='text' placeholder='motif de refus'/>
+    <textarea
+      className="resizable-textarea" // Apply resizable-textarea class for styling
+      value={text}
+      onChange={handleInputChange}
+      placeholder="Motif de refuse"
+    ></textarea>
    <div className="motifbtns">
       <button onClick={  ()=> closeMotif(false)}  className="motifcancel">Annuler</button>
        <button onClick={ handlesendClick}  className="motifenvoyer">Envoyer</button>
