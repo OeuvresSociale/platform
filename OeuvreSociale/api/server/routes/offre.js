22e6cb15d26bfb08d7180ce7d0af072d286f4ece
yousra <y.meflah@esi-sba.dz>
2024-03-24 21:35:22 +0000

    offre

--- OeuvreSociale/api/server/routes/offre.js
+++ OeuvreSociale/api/server/routes/offre.js
@@ -1,13 +1,12 @@
 const Router = require("express");
-
 const router=Router();
+const cntrl = require('../controllers/offreController');
 
 
-
-router.get("/offres",);
-router.get("/offre/:id",);
-router.post("/offre",addOffre);
-router.put("/offre/:id",);
-router.delete("/offre/:id",);
+router.get("/offres",cntrl.getOffres);
+router.get("/offre/:id",cntrl.getOffre);
+router.post("/offre",cntrl.addOffre);
+router.put("/offre/:id",cntrl.updateOffre);
+router.delete("/offre/:id",cntrl.deleteOffre);
 
 module.exports=router;
